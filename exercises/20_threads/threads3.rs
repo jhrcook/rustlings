9c9f552a951089023becfde679b438b9ff23d303
mo8it <mo8it@proton.me>
2024-03-18 01:12:37 +0100

    Remove unneeded Arc

--- exercises/20_threads/threads3.rs
+++ exercises/20_threads/threads3.rs
@@ -31,7 +31,7 @@ fn send_tx(q: Queue, tx: mpsc::Sender<u32>) -> () {
 
     let tx1 = tx.clone();
     thread::spawn(move || {
-        for val in &qc1.first_half {
+        for val in q.first_half {
             println!("sending {:?}", val);
             tx1.send(*val).unwrap();
             thread::sleep(Duration::from_secs(1));
@@ -40,7 +40,7 @@ fn send_tx(q: Queue, tx: mpsc::Sender<u32>) -> () {
 
     let tx2 = tx.clone();
     thread::spawn(move || {
-        for val in &qc2.second_half {
+        for val in q.second_half {
             println!("sending {:?}", val);
             tx2.send(*val).unwrap();
             thread::sleep(Duration::from_secs(1));
