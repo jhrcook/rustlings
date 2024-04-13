c4234762e6a70cbb689072fb567ab3f69f9a67d6
Joshua Cook <joshua_cook@vrtx.com>
2024-04-13 15:18:35 -0400

    solutions: macros

--- exercises/21_macros/macros2.rs
+++ exercises/21_macros/macros2.rs
@@ -3,7 +3,6 @@
 // Execute `rustlings hint macros2` or use the `hint` watch subcommand for a
 // hint.
 
-
 macro_rules! my_macro {
     () => {
         println!("Check out my macro!");
@@ -13,4 +12,3 @@ macro_rules! my_macro {
 fn main() {
     my_macro!();
 }
-
